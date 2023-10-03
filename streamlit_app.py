42249664ed0d895158139765b91b56fbe97f9790
anabusque <118977753+anabusque@users.noreply.github.com>
2023-10-03 14:11:01 +0200

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,7 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header('Fruityvice Fruit Advicce!')
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not_fruit_choice:
+  if not fruit_choice:
     streamlit.error('Please select a fruit to get information')
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
